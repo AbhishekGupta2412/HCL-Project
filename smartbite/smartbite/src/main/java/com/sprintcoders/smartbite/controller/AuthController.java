768ccbdfa3b9f890c7b57ffab1dac7cc1362c6d9
AbhishekGupta2412 <guptabhishek2412@gmail.com>
2026-02-26 12:23:36 +0530

    Controller Added

---
+++ smartbite/smartbite/src/main/java/com/sprintcoders/smartbite/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.sprintcoders.smartbite.controller;
+
+import com.sprintcoders.smartbite.entity.User;
+import com.sprintcoders.smartbite.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userService.login(user.getEmail(), user.getPassword());
+    }
+}
